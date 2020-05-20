616c1e5156a7a1dbe6521e3cfcc57b549d0b7432
Erik White <26148654+Erik-White@users.noreply.github.com>
2020-05-21 01:24:17 +0300

    Add extension methods for System.Drawing

---
+++ ESW.Extensions/Drawing/ImageInstance.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ESW.Extensions.Drawing
+{
+    /// <summary>
+    /// Extension methods for images stored as <see cref="Image"/> instances.
+    /// </summary>
+    public static class ImageInstance
+    {
+        /// <summary>
+        /// Convert an <see cref="Image"/> instance to a specified format and encoding.
+        /// </summary>
+        /// <param name="value">An <see cref="Image"/> instance</param>
+        /// <param name="format">The format for the image</param>
+        /// <param name="encoderParameters">A collection of <see cref="EncoderParameter"/>s to set image quality, colour depth etc</param>
+        /// <returns>An <see cref="Image"/> instance in the specified format and encoding</returns>
+        public static Image AsFormat(this Image value, ImageFormat format, EncoderParameters encoderParameters = null)
+        {
+            using (var ms = value.ToMemoryStream(format, encoderParameters))
+            {
+                return Image.FromStream(ms);
+            }
+        }
+
+        /// <summary>
+        /// Create a <see cref="byte"/> array from an <see cref="Image"/> instance.
+        /// </summary>
+        /// <param name="value">An <see cref="Image"/> instance</param>
+        /// <returns>A <see cref="byte"/> array</returns>
+        public static byte[] ToBytes(this Image value)
+        {
+            return value.ToBytes(value.RawFormat);
+        }
+
+        /// <summary>
+        /// Create a <see cref="byte"/> array from an <see cref="Image"/> instance.
+        /// </summary>
+        /// <param name="value">An <see cref="Image"/> instance</param>
+        /// <param name="format">The image format</param>
+        /// <returns>An image <see cref="byte"/> array in the specified format</returns>
+        public static byte[] ToBytes(this Image value, ImageFormat format)
+        {
+            return value.ToBytes(format);
+        }
+
+        /// <summary>
+        /// Create a <see cref="byte"/> array from an <see cref="Image"/> instance.
+        /// </summary>
+        /// <param name="value">An <see cref="Image"/> instance</param>
+        /// <param name="format">The image format</param>
+        /// <param name="encoderParameters">A collection of <see cref="EncoderParameter"/>s to set image quality, colour depth etc</param>
+        /// <returns>An image <see cref="byte"/> array in the specified format and encoding</returns>
+        public static byte[] ToBytes(this Image value, ImageFormat format, EncoderParameters encoderParameters = null)
+        {
+            using (var ms = value.ToMemoryStream(format, encoderParameters))
+            {
+                return ms.ToArray();
+            }
+        }
+    }
+}
